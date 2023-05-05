f1c395c14eeece7479da1423e78a73244b5ff3c5
Sindre Broch <sindrebroch@users.noreply.github.com>
2023-05-05 09:15:09 +0200

    Update __init__.py

--- custom_components/telenordrift/__init__.py
+++ custom_components/telenordrift/__init__.py
@@ -25,7 +25,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     hass.data[TELENORDRIFT_DOMAIN][entry.entry_id] = coordinator
 
-    hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
